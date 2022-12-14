bc889acf9dc99e6400faa9e570dfa1932a0b77e3
daviderl99 <daviderlich7@hot.ee>
2022-12-14 14:46:22 +0200

    login form

--- src/components/pages/Login/Login.tsx
+++ src/components/pages/Login/Login.tsx
@@ -1,6 +1,21 @@
+import { TextField, Button } from "@mui/material";
+import './Login.css';
 
 export default function Login() {
   return (
-    <h1>Login</h1>
+    <form className="form">
+      <TextField
+        id="email"
+        label="Email"
+      />
+      <TextField
+        id="password"
+        label="Password"
+        type="password"
+      />
+      <Button type="button" variant="contained" color="primary" className="form__btn">
+        Login
+      </Button>
+    </form>
   );
 }
